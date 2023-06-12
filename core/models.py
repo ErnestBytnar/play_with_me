be1b68d6509d54a0740d7373e7ef6399fba24502
Ernest Bytnar <ernestbytnar@gmail.com>
2023-06-12 21:55:08 +0200

    ax
    XxXx

---
+++ core/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+
+User = get_user_model()
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=True)
+    x = models.CharField(max_length=100, blank=True)
+    x1 = models.CharField(max_length=100, blank=True)
+    x2 = models.CharField(max_length=100, blank=True)
+    skill = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    caption = models.TextField()
+    description = models.CharField(max_length=1024, null=True)
+    description2 = models.CharField(max_length=1024, null=True)
+    description3 = models.CharField(max_length=1024, null=True)
+    description4 = models.CharField(max_length=1024, null=True)
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
